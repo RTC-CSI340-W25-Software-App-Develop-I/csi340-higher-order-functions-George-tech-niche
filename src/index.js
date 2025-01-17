d264bed14e750ae9dc872bf1b174228937c2f6fb
George-tech-niche <kinggeorgethe2@gmail.com>
2025-01-16 20:43:26 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,38 @@
 // 1. Introduction to Higher Order Functions
 // Callback function definition
+const foo = function() {
+  console.log("I’m a function!");
+};
 
+const boo = function(callback) {
+  console.log("Higher Order");
+  return callback();
+};
 
 // Higher Order Function definition
+const callback = function (){
+  console.log("callback function")
+};
+const higherOrder = function (foo) {
+  console.log("Higher Order Function");
+  return foo();
+};
+const brunchMenu =['apples', 'bananas', 'oranges', 'mangoes']
+const uppercaseMenu = brunchMenu.map(menuItem => menuItem.toUpperCase());
+console.log(uppercaseMenu);
+
+const brunchPrices = [12, 14, 26, 10, 18, 14];
+
+const cheaperMenuPrices = brunchPrices.filter(price => price < 17);
+
+console.log(cheaperMenuPrices);
+
 
 // Pass the Higher Order Function the Callback function as an argument
 
 
 // Example data for 2 and 3
-const brunchMenu = [
-  "eggs benedict",
-  "huevos rancheros",
-  "fried chicken & waffle",
-  "fried egg sandwich",
-];
-const brunchPrices = [15.0, 16.0, 18.0, 12.0];
+
 
 // 2. Built-in Higher Order Functions
 
